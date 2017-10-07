376adc48bc8ef07193a62df5813bc0636d2a1e42
Agustin Linari <ALINARI@gire.com>
2017-10-07 17:12:03 -0300

    datos perfil

--- web-admin/src/app/profile/profile.controller.js
+++ web-admin/src/app/profile/profile.controller.js
@@ -8,11 +8,15 @@
     function ProfileController(ProfileService, $state, $scope, $stateParams, $rootScope) {
         var vm = this;
         vm.error = false;
-        
+        vm.age = null;
+        vm.showLoader = true;
         vm.dataLoading = true;
+        vm.avatarLoading = true;
+
         ProfileService.GetById($stateParams.fbid).then(function (data) {
                 if (data.success) {
                     vm.profile = data.user;
+                    vm.age = getAge(data.user.birthday);
                     vm.dataLoading = false;
                 } else {
                     vm.error = true;
@@ -24,8 +28,10 @@
         ProfileService.GetImage($stateParams.fbid, 0).then(function (data) {
                 if (data.success) {
                     vm.avatar = data.image.data;
+                    vm.avatarLoading = false;
                 } else {
                     vm.error = true;
+                    vm.avatarLoading = true;
                     //Mostrar mensaje
                 }
         });
@@ -33,10 +39,22 @@
         ProfileService.GetImage($stateParams.fbid, 1).then(function (data) {
                 if (data.success) {
                     vm.image = data.image.data;
-                    
+                    vm.showLoader = false;
                 } else {
                     vm.error = true;
+                    vm.showLoader = false;
                     //Mostrar mensaje
                 }
         });
+
+        function getAge(dateString) {
+                        var today = new Date();
+                        var birthDate = new Date(dateString);
+                        var age = today.getFullYear() - birthDate.getFullYear();
+                        var m = today.getMonth() - birthDate.getMonth();
+                        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                            age--;
+                        }
+                        return age;
+                    }
     }
