a936dc54042235c356507593657c78f670c19361
Agustin Linari <ALINARI@gire.com>
2017-10-08 01:44:12 -0300

    Datos formateados en grilla de denuncias

--- web-admin/src/app/abuse/abuse.controller.js
+++ web-admin/src/app/abuse/abuse.controller.js
@@ -10,7 +10,9 @@
         vm.error = false;
         
         vm.dataLoading = true;
+        vm.buttonDisabled = false;
         vm.abuses = [];
+        
         AbuseService.GetAll().then(function (data) {
                 if (data.success) {
                     vm.abuses = data.abuseReports;
@@ -23,4 +25,38 @@
             });
 
         vm.itemsByPage=10;
+        vm.getCategory = function (category){
+            switch(category) {
+                case 1:
+                    return 'Fotos inapropiadas';
+                case 2:
+                    return 'Lenguaje Abusivo';
+                case 3:
+                    return 'Spam';
+                default:
+                    return 'Otros';
+            }
+        }
+
+        vm.getStatus = function (isOpen){
+            if (isOpen){
+                return 'Abierto';
+            }else{
+                return 'Cerrado';
+            }
+        }
+
+        vm.close = function(abuse){
+            vm.buttonDisabled = true;
+            AbuseService.Update(abuse._id, false).then(function (data) {
+                if (data.success) {
+                    abuse.isOpen = false;
+                    vm.buttonDisabled = false;
+                } else {
+                    vm.error = true;
+                    FlashService.Error(data.message);
+                    vm.buttonDisabled = false;
+                }
+            });
+        }
     }
